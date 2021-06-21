9b98eb6f82a4fdd9b2f664af891548a49578718d
erikkukamezou <erikkukamezoudayo@yahoo.co.jp>
2021-06-21 01:39:56 +0000

    詳細機能の追加

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,12 +1,20 @@
 class PicturesController < ApplicationController
   def index
+    @pictures = Picture.all
   end
   def new
     @picture =Picture.new
   end
   def create
     Picture.create(picture_params)
-    redirect_to new_picture_path
+    # @picture = current_user.picture.build(picture_params)
+    if @picture.save
+    redirect_to pictures_path, notice:"投稿しました！"
+  else
+    render :new
+  end
+  def show
+    @picture = Picture.find(params[:id])
   end
   private
   def picture_params
