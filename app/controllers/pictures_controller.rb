a72205ca9757d549c90b8f1230e7cfb343d7d273
erikkukamezou <erikkukamezoudayo@yahoo.co.jp>
2021-06-21 01:58:21 +0000

    削除機能の追加

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,5 @@
 class PicturesController < ApplicationController
+  before_action :set_picture, only: [:show, :edit, :update, :destroy]
   def index
     @pictures = Picture.all
   end
@@ -10,14 +11,33 @@ class PicturesController < ApplicationController
     # @picture = current_user.picture.build(picture_params)
     if @picture.save
     redirect_to pictures_path, notice:"投稿しました！"
-  else
+    else
     render :new
+    end
   end
   def show
-    @picture = Picture.find(params[:id])
+    # @picture = Picture.find(params[:id])
   end
+  def edit
+    # @picture = Picture.find(params[:id])
+  end
+  def update
+    # @picture = Picture.find(params[:id])
+    if @picture.update(picture_params)
+      redirect_to pictures_path, notice: "編集しました！"
+    else
+      render :edit
+    end
+  end
+  def destroy
+  @picture.destroy
+  redirect_to pictures_path, notice:"ブログを削除しました！"
+end
   private
   def picture_params
     params.require(:picture).permit(:title, :content)
+  end
+  def set_picture
+    @picture = Picture.find(params[:id])
   end
 end
